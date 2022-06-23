a3636a7b969cc0d524e54d4b4a7990036e79b415
FFeV <a.5920311@yandex.ru>
2022-06-24 00:28:39 +0300

    get_questions: print all pages

--- SOF.py
+++ SOF.py
@@ -1,18 +1,24 @@
 import requests
 from pprint import pprint
-from datetime import datetime, timedelta
+from time import time
 
 
 def get_questions(before_days, tag):
-    to_date = datetime.date(datetime.today())
-    from_date = to_date - timedelta(days=before_days)
+    to_date = int(time())
+    from_date = to_date - (86400 * before_days)
+    page, i = 1, 1
     url = 'https://api.stackexchange.com/2.3/questions'
-    params = {"fromdate": from_date, "todate": to_date, "order": "desc", "sort": "creation", "tagged": tag, "site": "stackoverflow"}
-    response = requests.get(url, params)
-    i = 1
-    for item in response.json()['items']:
-        print(i, item['title'])
-        i += 1
+    while True:
+        params = {"page": page, "pagesize": 99, "fromdate": from_date, "todate": to_date, "order": "desc", "sort": "creation", "tagged": tag, "site": "stackoverflow"}
+        response = requests.get(url, params)
+        questions_page = response.json()
+        for item in questions_page['items']:
+            print(i, item['title'])
+            i += 1
+        if questions_page["has_more"]:
+            page += 1
+        else:
+            break
     return
 
-get_questions(2, 'python')
+get_questions(1, 'python')
