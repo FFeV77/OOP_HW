20537c9be0851bb39b2f05ae9e3ddc0c256f4234
Andrey <105676336+FFeV77@users.noreply.github.com>
2022-08-25 00:58:56 +0300

    Update SQL_SELECT.sql

--- SQL_SELECT.sql
+++ SQL_SELECT.sql
@@ -20,56 +20,53 @@ WHERE name LIKE '%мой%' OR name LIKE '%my%';
 
 
 SELECT name, COUNT(genre_id) FROM genre g
-JOIN artistgenre ag ON g.id=ag.genre_id
-GROUP BY g.id;
+JOIN artistgenre ag USING(genre_id)
+GROUP BY genre_id;
 
-SELECT a.name, COUNT(album_id) FROM album a
-LEFT JOIN single s ON a.id=s.album_id
+SELECT a.name, COUNT(s.album_id) FROM album a
+LEFT JOIN single s USING(album_id)
 WHERE a.year BETWEEN 2019 AND 2020
-GROUP BY a.id;
+GROUP BY a.album_id
 
 SELECT a.name, AVG(s.duration) FROM album a
-JOIN single s ON a.id=s.album_id
-GROUP BY a.id;
+JOIN single s USING(album_id)
+GROUP BY a.album_id;
 
-SELECT a.name FROM artist a
-JOIN artistalbum aa ON a.id=aa.artist_id
-JOIN album am ON am.id=aa.album_id
-WHERE am.year NOT IN (2020)
-GROUP BY a.id;
+SELECT name FROM artist
+WHERE artist_id NOT IN (
+	SELECT DISTINCT a.artist_id FROM artist a
+	JOIN artistalbum aa USING(artist_id)
+	JOIN album am USING(album_id)
+	WHERE am.year IN (2020))
 
-SELECT c.name FROM collection c
-JOIN collectionsingle cs ON c.id=cs.collection_id
-JOIN single s ON cs.single_id=s.id
-JOIN artistalbum aa ON s.album_id=aa.album_id
-JOIN artist a ON aa.artist_id=a.id
+SELECT DISTINCT c.name FROM collection c
+JOIN collectionsingle cs USING(collection_id)
+JOIN single s USING(single_id)
+JOIN artistalbum aa USING(album_id)
+JOIN artist a USING(artist_id)
 WHERE a.name LIKE 'Metallika'
-GROUP BY c.id;
 
-SELECT name FROM (
-SELECT a.name, count(ag.genre_id)>1 c FROM album a
-JOIN artistalbum aa ON a.id=aa.album_id
-JOIN artistgenre ag ON aa.artist_id=ag.artist_id
-GROUP BY a.id
-) al_g WHERE al_g.c = TRUE;
+SELECT a.name FROM album a
+JOIN artistalbum aa USING(album_id)
+JOIN artistgenre ag USING(artist_id)
+GROUP BY a.album_id
+HAVING COUNT(DISTINCT genre_id) > 1;
 
 SELECT s.name FROM single s
-LEFT JOIN collectionsingle cs ON s.id=cs.single_id
+LEFT JOIN collectionsingle cs USING(single_id)
 WHERE cs.single_id IS NULL;
 
 SELECT a.name FROM artist a
-JOIN artistalbum aa ON a.id=aa.artist_id
-JOIN single s ON aa.album_id=s.album_id
+JOIN artistalbum aa USING(artist_id)
+JOIN single s USING(album_id)
 WHERE s.duration = (SELECT min(duration) FROM single)
-GROUP BY a.id;
+GROUP BY a.artist_id;
 
-SELECT aa.name FROM (
-SELECT a.name, count(s.album_id) FROM single s
-JOIN album a ON s.album_id=a.id
-GROUP BY a.id
-) aa
-WHERE count = (
-SELECT min(aa.count) FROM (
-SELECT count(album_id) FROM single 
+WITH res AS (
+SELECT a.name, COUNT(single_id) track_count FROM album a
+JOIN single s USING(album_id)
 GROUP BY album_id
-) aa );
+)
+SELECT res.name FROM res
+WHERE res.track_count = (
+	SELECT min(res.track_count) FROM res)
